8934179aa5a0fa115abf265d288ac513aa8f14d0
johanrussouw <jrussouw610505@gmail.com>
2026-03-12 17:38:33 +0200

    added login and signup forms

--- src/components/web/navbar.tsx
+++ src/components/web/navbar.tsx
@@ -1,6 +1,10 @@
+import { Link } from '@tanstack/react-router'
+import { buttonVariants } from '../ui/button'
+import { ThemeToggle } from './theme-toggle'
+
 export function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 border-b">
+    <nav className="sticky top-0 z-50 border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
       <div className="mx-auto flex h-16 max-w-6xl items-center justify-between px-4">
         <div className="flex items-center gap-2">
           <img
@@ -11,9 +15,16 @@ export function Navbar() {
           <h1 className="text-lg font-semibold">TanStack Start</h1>
         </div>
         <div className="flex items-center gap-3">
-          <button className="text-white bg-red-500 px-3 py-1 rounded-lg">
-            Sign Up
-          </button>
+          <Link to="/login" className={buttonVariants({ variant: 'outline' })}>
+            Login
+          </Link>
+          <Link
+            to="/signup"
+            className={buttonVariants({ variant: 'secondary' })}
+          >
+            Get started
+          </Link>
+          <ThemeToggle />
         </div>
       </div>
     </nav>
